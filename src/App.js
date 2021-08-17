a0537fb84454b684041cf3bca63e29ff0318598b
Astitva7717 <astitvaaggarwal7717@gmail.com>
2021-08-17 19:25:03 +0530

    logo and bootstrap

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">hello folks</div>
+    )
+  }
+}
+
+export default App;
